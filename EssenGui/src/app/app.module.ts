88bc18c113745577664221033ccb467c55a00b15
Stefan Hoffmeister <hoffmeister.stefan99@gmail.com>
2023-05-31 19:13:49 +0200

    more refactoring and styling chagnes

--- EssenGui/src/app/app.module.ts
+++ EssenGui/src/app/app.module.ts
@@ -6,16 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { CategoryListComponent } from './pages/category-list/category-list.component';
 import { CategoryDetailsComponent } from './pages/category-details/category-details.component';
 import { RecipeAddComponent } from './pages/recipe-add/recipe-add.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-import { RecipeDisplayComponent } from './pages/recipe-display/recipe-display.component';
 import { EditDialogComponent } from './components/edit-dialog/edit-dialog.component';
 import { DeleteButtonComponent } from './components/delete-button/delete-button.component';
 import { EditButtonComponent } from './components/edit-button/edit-button.component';
 import { TextButtonComponent } from './components/text-button/text-button.component';
+import { RecipeListComponent } from './pages/recipe-list/recipe-list.component';
+import { RecipeDisplayComponent } from './components/recipe-display/recipe-display.component';
 
 
 
